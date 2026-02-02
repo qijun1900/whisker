d987382dfd07176e948b73d3addad16bc93ad0ad
qijun1900 <3308837335@qq.com>
2026-02-02 20:33:11 +0800

    更新 manifest.json 中的图标路径，添加内容页面和样式文件

---
+++ src/contentPage/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref('Content Page')
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <p>这是内容页面</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  color: #42b983;
+}
+</style>
