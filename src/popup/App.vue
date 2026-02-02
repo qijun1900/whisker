fad2cce1c4ad48862a77856b410ff4a13510cb07
qijun1900 <3308837335@qq.com>
2026-02-02 20:08:06 +0800

    init

---
+++ src/popup/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref('Hello Whisker!')
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <p>Welcome to your extension popup.</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 20px;
+  min-width: 300px;
+  text-align: center;
+}
+</style>
